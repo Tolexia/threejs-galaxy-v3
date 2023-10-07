b3a337310da563ea8a9cc66c03883d5520b6e471
Axel C <69471505+Tolexia@users.noreply.github.com>
2023-10-07 17:19:03 +0200

    removed script to update positions : too many points, need instancedMesh to be used in v4

--- main.js
+++ main.js
@@ -30,12 +30,7 @@ parameters.outsideColor = '#99edf7'
 
 let 
     points,
-    pointsIndexed,
-    pointsIndexedOffset,
-    pointclouds,
     geometry,
-    geometryIndexed,
-    geometryIndexedWithOffset,
     material,
     canvas,
     renderer,
@@ -43,20 +38,13 @@ let
     camera,
     orbit,
     composer,
-    bloomComposer,
-    overlayComposer,
-    baseComposer,
-    controls,
     raycaster,
     pointer,
-    sphere,
     isClicking,
     positions,
-    positionsInit,
     colors,
     sizes,
     intersects,
-    INTERSECTED,
     vertices
 
 const generateGalaxy = ( ) =>
@@ -116,44 +104,27 @@ const generateGalaxy = ( ) =>
         // Size
         sizes[i] = parameters.size;
     }
-    positionsInit = Object.assign({}, positions);
-
+ 
     geometry.setAttribute( 'position', new THREE.BufferAttribute(positions, 3) )
+    // geometry.setFromPoints(vertices)
     geometry.setAttribute( 'customColor', new THREE.BufferAttribute( colors, 3 ) );
     geometry.setAttribute( 'size', new THREE.BufferAttribute( sizes, 1 ) );
 
-    const spriteTexture = new THREE.TextureLoader().load( './resources/sprite120.png' );    
     /**
      * Material
      */
-    //  material = new THREE.PointsMaterial({
-    //     // size: parameters.size,
-    //     sizeAttenuation: true,
-    //     depthWrite: false,
-    //     blending: THREE.AdditiveBlending,
-    //     // vertexColors: true,
-    //     // color:0xFFFFFF,
-    //     alphaMap: spriteTexture,
-    //     transparent: true,
-    // })
-    material = new THREE.ShaderMaterial( {
-
-        uniforms: {
-            color: { value: new THREE.Color( 0xffffff ) },
-            pointTexture: { value: new THREE.TextureLoader().load( './resources/sprite120.png' ) },
-            alphaTest: { value: 0.9 }
-        },
-        vertexShader: document.getElementById( 'vertexshader' ).textContent,
-        fragmentShader: document.getElementById( 'fragmentshader' ).textContent
-
-    } );
+     material = new THREE.PointsMaterial({
+        sizeAttenuation: true,
+        depthWrite: false,
+        blending: THREE.AdditiveBlending,
+        color:0xFFFFFF,
+        transparent: true,
+    })
     /**
     * Points
     */
    points = new THREE.Points(geometry, material)
    scene.add(points)
-   pointclouds = points;
-
 }
     
 /**
@@ -184,7 +155,7 @@ function initRender() {
     renderer = new THREE.WebGLRenderer({
         antialias: true,
         canvas,
-        // logarithmicDepthBuffer: true,
+        logarithmicDepthBuffer: true,
     })
     renderer.setPixelRatio( window.devicePixelRatio )
     renderer.setSize( window.innerWidth, window.innerHeight )
@@ -211,18 +182,6 @@ function init()
 {
     canvas = document.querySelector('canvas.webgl')
     
-    // const cubeTextureLoader = new THREE.CubeTextureLoader();
-    // const environmentMapTexture = cubeTextureLoader.load(
-    //   [
-    //     'https://closure.vps.wbsprt.com/files/earth/space/px.png',
-    //     'https://closure.vps.wbsprt.com/files/earth/space/nx.png',
-    //     'https://closure.vps.wbsprt.com/files/earth/space/py.png',
-    //     'https://closure.vps.wbsprt.com/files/earth/space/ny.png',
-    //     'https://closure.vps.wbsprt.com/files/earth/space/pz.png',
-    //     'https://closure.vps.wbsprt.com/files/earth/space/nz.png',
-    //   ],
-    // );
-    // scene.background = environmentMapTexture;
     scene = new THREE.Scene();
     scene.add( new THREE.AmbientLight( 0xcccccc  ) );
     scene.fog = new THREE.FogExp2(0xEBE2DB, 0.00003);
@@ -245,13 +204,7 @@ function init()
     orbit.maxDistance = 16384;
     orbit.maxPolarAngle = (Math.PI / 2) - (Math.PI / 360)
 
-    raycaster = new THREE.Raycaster()
     pointer = new THREE.Vector2()
-    const sphereGeometry = new THREE.SphereGeometry( 1, 32, 32 );
-    // const sphereMaterial = new THREE.MeshBasicMaterial( { transparent:true, opacity:1 } );
-    const sphereMaterial = new THREE.MeshBasicMaterial( { color:0xffffff} );
-    sphere = new THREE.Mesh( sphereGeometry, sphereMaterial );
-    scene.add( sphere );
 
     initRender()
 }
@@ -263,16 +216,6 @@ window.addEventListener('pointermove', (event) => {
 })
 function renderPipeline() {
 
-    // Render bloom
-    // camera.layers.set(BLOOM_LAYER)
-    // bloomComposer.render()
-
-    // Render overlays
-    // camera.layers.set(OVERLAY_LAYER)
-    // overlayComposer.render()
-
-    // Render normal
-    // camera.layers.set(BASE_LAYER)
     composer.render();
     // renderer.render(scene, camera)
 }
@@ -307,66 +250,7 @@ async function render() {
     camera.aspect = canvas.clientWidth / canvas.clientHeight;
     camera.updateProjectionMatrix();
     const elapsedTime = clock.getElapsedTime()
-    // points.rotation.z = elapsedTime * 0.05
-    // galaxy.updateScale(camera)
-    const geometry = points.geometry;
-    const attributes = geometry.attributes;
-    let pos = attributes.position;
-    // console.log("attributes", attributes);
-    // let vUv = new THREE.Vector2();
-    // Raycaster
-    // update the picking ray with the camera and pointer position
-    raycaster.setFromCamera( pointer, camera );
-    intersects = raycaster.intersectObject( pointclouds );
-
-    if ( intersects.length > 0 ) {
-
-        if ( INTERSECTED != intersects[ 0 ].index ) {
-            // console.log("vertices[INTERSECTED]", vertices[INTERSECTED]);
-            // pos.array[ INTERSECTED ] = vertices[INTERSECTED].y;
-
-            INTERSECTED = intersects[ 0 ].index;
-            console.log("intersects[ 0 ]", intersects[ 0 ]);
-            // console.log("pos.array[ INTERSECTED ]", pos.array[ INTERSECTED ]);
-            pos.array[ INTERSECTED ] = vertices[INTERSECTED].z + 0.5;
-
-            pos.needsUpdate = true;
-
-        }
-
-    } else if ( INTERSECTED !== null ) {
-
-        // attributes.size.array[ INTERSECTED ] = parameters.size;
-        // attributes.size.needsUpdate = true;
-        // attributes.position = positionsInit
-        // attributes.position.needsUpdate = true;
-        // INTERSECTED = null;
-    }
-
-    const intersections = raycaster.intersectObject( pointclouds );
-    const intersection = ( intersections.length ) > 0 ? intersections[ 0 ] : null;
-    // if ( intersection !== null && isClicking == true) 
-    if ( intersection !== null) 
-    {
-        // console.log("intersection", intersection);
-        // console.log("intersection.point", intersection.point);
-        // console.log("intersection.index", intersection.index);
-        // sphere.position.copy( intersection.point );
-        // sphere.scale.set( 0.1, 0.1, 0.1 );
-        // console.log("attributes.customColor.array[intersection.index]", attributes.customColor.array[intersection.index]);
-        // attributes.customColor.array[intersection.index] = 0x00ff0f
-        // attributes.customColor.array[intersection.index+1] = 0x00ff0f
-        // attributes.customColor.array[intersection.index+2] = 0x00ff0f
-        // attributes.customColor.needsUpdate = true;
-        // attributes.size.array[ intersection.index ] = parameters.size * 20;
-        // console.log("attributes.size.array[ intersection.index ]", attributes.size.array[ intersection.index ]);
-        // attributes.size.needsUpdate = true;
-
-    }
-    // else
-    // {
-    //     sphere.position.copy( new THREE.Vector3(0,0,0));
-    // }
+    points.rotation.z = elapsedTime * 0.05
 
     // Run each pass of the render pipeline
     renderPipeline()
